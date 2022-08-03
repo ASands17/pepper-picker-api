4fad4ea0c38ab744f9001c6462fd069a9e5565b7
Amanda Sands <amandajean555@gmail.com>
2022-08-03 12:35:57 -0600

    Add peppers to first endpoint

--- data/peppers.data.js
+++ data/peppers.data.js
@@ -28,6 +28,66 @@ const peppers = [
         name: 'Leysa',
         spiceLevel: 'mild',
         imageUrl: 'https://peppadogs.com/wp-content/uploads/lesya_pepper_seeds.jpg'
+    },
+    {
+        id: 6,
+        name: 'Alma Paprika',
+        spiceLevel: 'medium',
+        imageUrl: 'https://www.superhotchiles.com/wp-content/uploads/2018/04/Alma-Paprika-Pepper-seeds.jpg'
+    },
+    {
+        id: 7,
+        name: 'Poblano',
+        spiceLevel: 'mild',
+        imageUrl: 'https://img.ehowcdn.com/640/photos.demandstudios.com/137/114/fotolia_292007_XS.jpg'
+    },
+    {
+        id: 8,
+        name: 'Cayenne',
+        spiceLevel: 'medium',
+        imageUrl: 'https://www.gardeningknowhow.com/wp-content/uploads/2020/11/cayenne-peppers.jpg'
+    },
+    {
+        id: 9,
+        name: 'Scotch Bonnet',
+        spiceLevel: 'very hot',
+        imageUrl: 'https://www.bhg.com/thmb/Dyg-SW66dLlM0sAZD-5WMyewjnU=/650x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/scotch-bonnet-hot-pepper-growing-1pgTBblWqwABI9Hjn2WcVU-a3961d458de54a6eb38d3f8ced5f9645.jpg'
+    },
+    {
+        id: 10,
+        name: 'Sweet Banana',
+        spiceLevel: 'mild',
+        imageUrl: 'https://cdn.shopify.com/s/files/1/0257/4565/3811/products/sweet-banana-pepper.jpg?v=1642629950'
+    },
+    {
+        id: 11,
+        name: 'Tabasco',
+        spiceLevel: 'hot',
+        imageUrl: 'http://cdn.shopify.com/s/files/1/1122/4336/products/tabascopeppr_1024x1024.jpg?v=1456156751'
+    },
+    {
+        id: 12,
+        name: 'Mad Hatter',
+        spiceLevel: 'mild',
+        imageUrl: 'https://all-americaselections.org/wp-content/uploads/2016/11/Mad-Hatter-Sweet-Pepper__PEP16-21910.jpg'
+    },
+    {
+        id: 13,
+        name: 'Fish Pepper',
+        spiceLevel: 'medium',
+        imageUrl: 'https://assets.epicurious.com/photos/6065d92cb0168183c7f5c99e/6:4/w_1600,c_limit/fish_pepper_IG_V1_VOG_final.jpg'
+    },
+    {
+        id: 14,
+        name: 'Habanada',
+        spiceLevel: 'mild',
+        imageUrl: 'https://a6ciswdyxn-flywheel.netdna-ssl.com/wp-content/uploads/2015/03/Habanada-The-Unmasked-Habanero3-1oo1rxd.jpg'
+    },
+    {
+        id: 15,
+        name: 'Serrano',
+        spiceLevel: 'medium',
+        imageUrl: 'http://www.thedailygarden.us/uploads/4/5/4/9/45493619/editor/serano.png?1599237015'
     }
 ]
 
